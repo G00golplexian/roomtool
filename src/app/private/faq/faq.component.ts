b3cc316064726c62f29f49cfde0bc231a0defcdf
Pierre Anton <p.anton@gso.schule.koeln>
2022-11-10 16:24:50 +0100

    checkpoint helios

--- src/app/private/faq/faq.component.ts
+++ src/app/private/faq/faq.component.ts
@@ -10,16 +10,16 @@ export class FaqComponent implements OnInit {
 
   faqs: Faq[] = [
     {
-      question: "Wie wird das Wetter heute?",
-      answer: "Nicht besonders gut."
+      question: "Wie erstelle ich einen Account?",
+      answer: "Auf der Startseite der App den Button 'Registrieren' drücken und deine E-Mail-Adresse mit den gewünschten Daten eingeben. Anschließend auf den Button 'Registrieren' klicken."
     },
     {
-      question: "Wie wird das Wetter morgen?",
-      answer: "Besser."
+      question: "Wie setze ich mein Passwort zurück?",
+      answer: "Im nicht eingeloggten Zustand auf den Button 'Passwort vergessen?' drücken und danach deine E-Mail-Adresse angeben. Anschließend auf den Button 'Absenden' klicken. Dann erhältst du eine E-Mail mit deinem neuen Passwort (ggf. in den Spam-Ordner gucken). Im eingeloggten Zustand kann das Passwort unter 'Mein Profil' erneuert werden."
     },
     {
-      question: "Wie wird das Wetter nächste Woche?",
-      answer: "Keine Ahnung."
+      question: "Wie lege ich eine neue Defekt-Meldung an?",
+      answer: "Im eingeloggten Zustand auf den Menüpunkt 'Defekt melden' klicken und das Formular ausfüllen. Anschließend auf den Button 'Absenden' klicken."
     },
   ]
 
